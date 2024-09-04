ca66e68ff3f0e25b0087ddc58e5c51d0913e0a4b
maifervc89 <maifervc89@gmail.com>
2024-09-04 19:07:13 -0400

    tema 5, recepcion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { ProveedoresModule } from './proveedores/proveedores.module';
 import { ProductosModule } from './productos/productos.module';
 import { AppWrapperComponent } from './app-wrapper/app-wrapper.component';
 import { ReservaTurnosComponent } from './reserva-turnos/reserva-turnos.component';
-
+import { TurnoListComponent } from './recepcion-productos/turno-list/turno-list.component';
 const routes: Routes = [
     { path: '', component: AppWrapperComponent, children: [
     { path: 'proveedores', loadChildren: () => import('./proveedores/proveedores.module').then(m => m.ProveedoresModule) },
     { path: 'productos', loadChildren: () => import('./productos/productos.module').then(m => m.ProductosModule) },
     { path: 'jaulas', loadChildren: () => import('./jaulas/jaulas.module').then(m => m.JaulasModule) },
     { path: 'reserva-turnos', component: ReservaTurnosComponent },
-    ]},
+    { path: 'recepcion', loadChildren: () => import('./recepcion-productos/recepcion-productos.module').then(m => m.RecepcionProductosModule) },
+  ]},
   /* { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }, */
 ];
